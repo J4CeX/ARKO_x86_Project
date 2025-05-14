2fa6511e4a60d6d6a3e562daae1ed67c60b46243
Jacek <jacek_kozakowski@wp.pl>
2025-05-15 00:08:07 +0200

    Implemented esp+0 and esp+4

--- colormask.s
+++ colormask.s
@@ -7,6 +7,7 @@ colormask:
     push ebx
     push esi
     push edi
+    sub esp, 8
 
     ; [ebp + 8] = img (void *)
     ; [ebp + 12] = width (uint32_t)
@@ -23,22 +24,29 @@ colormask:
     mov ecx, [ebp + 8]      ; ecx = img
     mov edx, [ebp + 20]     ; edx = mask_img
     mov esi, [ebp + 32]     ; esi = x
-    mov eax, [ebp + 36]     ; eax = y
+    mov edi, [ebp + 36]     ; edi = y
+
+    mov dword [esp + 0], 0
+    mov dword [esp + 4], 0
 
 mask:
-    mov ebx, [ebp + 12]     ; width
-    cmp esi, ebx            ; if x = width then next_row
+    mov eax, [ebp + 24]     ; eax = mask_width
+    cmp esi, eax            ; if x = mask_width then next_row
     je next_row
 
-    mov eax, [ebp + 36]     ; eax = y
-    mov ebx, [ebp + 28]     ; ebx = mask_height
-    dec ebx                 ; ebx -= 1
-    sub ebx, eax            ; mask_y = mask_height - 1 - y
+    mov eax, [ebp + 12]     ; eax = width
+    cmp esi, eax            ; if x = width then next_row
+    je next_row
 
-    mov eax, [ebp + 24]     ; mask_width
-    imul ebx, eax           ; mask_y * mask_width
+    mov ebx, [ebp + 16]     ; ebx = height
+    dec ebx                 ; ebx -= 1
+    sub ebx, edi            ; mask_y = height - 1 - y
+
+    mov eax, [ebp + 12]     ; width
+    imul ebx, eax           ; mask_y * width
     add ebx, esi            ; + x
     imul ebx, 3             ; offset w bajtach
+
 
     ; kopiowanie koloru z maski do obrazu
     mov al, [edx + ebx]     ; save B of pixel in mask_img to al
@@ -53,15 +61,20 @@ mask:
 
 next_row:
     mov esi, [ebp + 32]     ; set current x as given x
-    mov eax, [ebp + 36]     ; eax = y
-    inc eax                 ; inc y
-    mov [ebp + 36], eax     ; save y
+    inc edi                 ; inc y
 
     mov ebx, [ebp + 16]     ; height
-    cmp eax, ebx            ; if y = height then mask
+    cmp edi, ebx            ; if y = height then mask else exit
     jl mask
+    jge exit
+
+    mov ebx, [ebp + 28]     ; mask_height
+    cmp edi, ebx            ; if y = mask_height then mask else exit
+    jl mask
+    jge exit
 
 exit:
+    add esp, 8
     pop edi
     pop esi
     pop ebx
